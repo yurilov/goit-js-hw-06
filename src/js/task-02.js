45264701a85db7cc3a8b5a5376546f512d1a2a89
Yurii Shchepilov <yuraschepilov@gmail.com>
2021-12-27 15:54:33 +0200

    completed second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListRef = document.querySelector("#ingredients");
+
+const ingredientsList = ingredients.map((ingredient) => {
+  const ingredientsListItemRef = document.createElement("li");
+  ingredientsListItemRef.className = "item";
+  ingredientsListItemRef.textContent = ingredient;
+
+  return ingredientsListItemRef;
+});
+
+ingredientsListRef.append(...ingredientsList);
