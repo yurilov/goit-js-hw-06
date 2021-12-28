c6c84a8657790a27797281f2ef8868a59d82933f
Yurii Shchepilov <yuraschepilov@gmail.com>
2021-12-28 22:02:44 +0200

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,3 +2,14 @@
   /* <input type="text" id="name-input" placeholder="Please enter your name" />
 <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */
 }
+
+const nameInputRef = document.querySelector("#name-input");
+
+const nameOutputRef = document.querySelector("#name-output");
+nameOutputRef.textContent = "Anonymous";
+nameInputRef.addEventListener("input", (event) => {
+  if (event.currentTarget.value == "") {
+    nameOutputRef.textContent = "Anonymous";
+  }
+  nameOutputRef.textContent = event.currentTarget.value;
+});
