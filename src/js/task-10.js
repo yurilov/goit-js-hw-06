e2e9982a9400da2ba0729ea4f9088183aa99b5f2
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2021-12-30 10:55:36 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const createBoxesBtn = document.querySelector('button[data-create]');
+const destroyBoxesBtn = document.querySelector('button[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+function createBoxes() {
+  const boxes = [];
+  let initialSize = 30;
+  
+  for (let i = 1; i<= input.value; i += 1) {
+    initialSize = i > 1 ? initialSize + 10 : initialSize;
+    const color = getRandomHexColor();
+    boxes.push(`<div style="width: ${initialSize}px; height: ${initialSize}px; background-color: ${color}"></div>`);
+  }
+  
+  const joinedBoxes = boxes.join('');
+  input.value = '';
+  return boxesRef.insertAdjacentHTML('beforeend', joinedBoxes);
+}
+
+function destroyBoxes() {
+  return boxesRef.innerHTML = '';
+
+}
+
+createBoxesBtn.addEventListener('click', createBoxes);
+
+destroyBoxesBtn.addEventListener('click', destroyBoxes);
