66b9740b70e802de5ffedb77d07f022d224322b7
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2021-12-30 09:00:32 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,9 +1,10 @@
 const input = document.querySelector("#validation-input");
+const inputLength = Number(input.dataset.length);
 
 input.addEventListener("blur", isInputValid);
 
 function isInputValid() {
-  if (input.value.length === 6) {
+  if (input.value.length === inputLength) {
     input.classList.add("valid");
     input.classList.remove("invalid");
     return;
