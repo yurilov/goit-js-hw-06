77a8101a6d688b907e6f911ee55a2b15bb69f527
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2021-12-30 09:29:19 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+  
+  const elements = {
+    email: email.value,
+    password: password.value,
+  }
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+  console.log(elements);
+  event.currentTarget.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
