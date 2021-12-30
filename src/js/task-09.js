00a28c365f2f937cc1ad7ed52e3dbf0e6678a123
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2021-12-30 09:55:40 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+changeColorBtn.addEventListener('click',changeBgColor);
